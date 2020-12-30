8a36e1ee693aba5e289f69c57725662825dc2c97
C-ArenA <carlostata.ca@gmail.com>
2020-12-29 21:04:05 -0400

    I have a function that can determine whether or not the current player wins

--- main.js
+++ main.js
@@ -21,9 +21,33 @@ for (let i = 0; i < buttonsArray.length; i++) {
 // H: 0-1-2, 3-4-5, 6-7-8, 
 // V: 0-3-6, 2-5-8, 1-4-7,
 // D: 0-4-8, 2-4-6 
-function checkGame() {
+function checkIfWins() {
+    let toWin = player + player + player;
+    let result = "";
     for (let i = 0; i < buttonsArray.length; i++) {
         const button = buttonsArray[i];
-        
+        result += button.innerHTML; 
     }
+    // Check H:
+    for (let h = 0; h < result.length; h+=3) {
+        const toTest = result.slice(h, h+3);
+        if (toTest == toWin) return true;
+    }
+    // Check V:
+    for (let v = 0; v < 3; v++) {
+        const toTest = result[v] + result[v+3] + result[v+6];
+        if (toTest == toWin) return true;
+    }
+    // Check D \:
+    {
+        const toTest = result[0] + result[4] + result[8];
+        if (toTest == toWin) return true;
+    }
+    // Check D /:
+    {
+        const toTest = result[2] + result[4] + result[6];
+        if (toTest == toWin) return true;
+    }
+    return false;
 }
+
