ede31a925d4203e17ec1b861d4dc254c602172b2
C-ArenA <carlostata.ca@gmail.com>
2020-12-29 22:08:39 -0400

    Había olvidado poner una acción ante el empate XD

--- main.js
+++ main.js
@@ -5,12 +5,17 @@
 // H: 0-1-2, 3-4-5, 6-7-8, 
 // V: 0-3-6, 2-5-8, 1-4-7,
 // D: 0-4-8, 2-4-6 
-function wins(player, buttonsArray) {
+function checkIfWins(player, buttonsArray) {
     let toWin = player + player + player;
     let result = "";
+    let tie = 0;
     for (let i = 0; i < buttonsArray.length; i++) {
         const button = buttonsArray[i];
+        if (button.innerHTML != "-") tie = tie + 1;
         result += button.innerHTML; 
+    }
+    if (tie >= 9) {
+        return undefined;
     }
     // Check H:
     for (let h = 0; h < result.length; h+=3) {
@@ -48,8 +53,14 @@ function initializeGame() {
         button.addEventListener("click", () => {
             if (button.innerHTML != "-") return false;
             button.innerHTML = player;
-            if (wins(player, buttonsArray)) {
-                info.innerHTML = "Player " + player + " Wins!!"; 
+            let wins = checkIfWins(player, buttonsArray);
+            if(wins === undefined) {
+                message.innerHTML = "It's a tie :("; 
+                start.value = "Play Again?"
+                askingUser.style["display"] = "block";
+                return true;
+            }
+            if (wins) {
                 message.innerHTML = "Player " + player + " Wins!!"; 
                 start.value = "Play Again?"
                 askingUser.style["display"] = "block";
